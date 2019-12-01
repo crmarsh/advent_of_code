fde16d36ecb851051b22630d4b3a4010ee6f0323
Chris Marsh <c.marshian@gmail.com>
2019-11-30 22:34:31 -0800

    2015 day 15 part 1

---
+++ 2015/day15/code.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import operator
+import os
+import functools
+import itertools
+
+here = os.path.dirname(__file__)
+input_path = os.path.join(here, 'input.txt')
+
+total_teaspoons = 100
+additive_properties = ['capacity', 'durability', 'flavor', 'texture']
+
+def score_ingredient(props):
+    capacity = props['capacity']
+    durability = props['durability']
+    flavor = props['flavor']
+    texture = props['texture']
+    return capacity, durability, flavor, texture
+
+
+def load_input():
+    # Frosting: capacity 4, durability -2, flavor 0, texture 0, calories 5
+    ingredients = []
+    with open(input_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            name, properties = line.split(': ')
+            properties = properties.split(', ')
+            properties = {
+                entry[0]: int(entry[1])
+                for entry in [x.split(' ') for x in properties]
+            }
+            ingredients.append((name, properties))
+    return ingredients
+
+
+def my_partition(n, d):
+    for entry in itertools.combinations_with_replacement(range(d), n):
+        res = [0] * d
+        for item in entry:
+            res[item] += 1
+        yield res
+
+
+def main():
+    ingredients = load_input()
+    print(ingredients)
+    n = len(ingredients)
+    for name,props in ingredients:
+        print(name, score_ingredient(props))
+
+    best_score = 0
+    best = None
+    
+    for trial in my_partition(total_teaspoons, n):
+        prop_values = {prop_name: 0 for prop_name in additive_properties}
+        for i,amount in enumerate(trial):
+            for prop_name in additive_properties:
+                prop_values[prop_name] += amount * ingredients[i][1][prop_name]
+        vals = prop_values.values()
+        if any([x < 0 for x in vals]):
+            score = 0
+        else:
+            score = functools.reduce(operator.mul, prop_values.values(), 1)
+        
+        if score > best_score:
+            best_score = score
+            best = trial
+            print(trial, score)
+    print(best_score, best)
+
+
+
+if __name__ == '__main__':
+    main()

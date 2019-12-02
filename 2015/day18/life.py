6e8bca045f89113f48b670042bce82eab4a92dc7
Chris Marsh <c.marshian@gmail.com>
2019-12-01 17:50:57 -0800

    2015 day 18 p 2

--- 2015/day18/life.py
+++ 2015/day18/life.py
@@ -33,27 +33,47 @@ def board_sum(board):
     return sum(sum(board))
 
 
+def iter_neighbors_indicies(board, row, col):
+    start_row = max(0, row - 1)
+    end_row = min(row + 2, board.shape[0])
+    start_col = max(0, col - 1)
+    end_col = min(col + 2, board.shape[1])
+    for nr in range(start_row, end_row):
+        for nc in range(start_col, end_col):
+            if (nr,nc) == (row,col):
+                continue
+            yield nr,nc
+
+
+def count_neighbors(board, row, col):
+    total = 0
+    for i in iter_neighbors_indicies(board, row, col):
+        total += board[i]
+    return total
+
+
 def step_board(prev_board):
     new_board = np.zeros_like(prev_board)
     for row in range(new_board.shape[0]):
         for col in range(new_board.shape[1]):
-            start_row = max(0, row - 1)
-            end_row = min(row + 2, new_board.shape[0])
-            start_col = max(0, col - 1)
-            end_col = min(col + 2, new_board.shape[1])
-            neighbors = prev_board[start_row:end_row, start_col:end_col]
-            count = board_sum(neighbors)
-            if prev_board[row, col] == 1 and 3 <= count <= 4:
+            count = count_neighbors(prev_board, row, col)
+            if count == 3:
                 new_board[row, col] = 1
-            elif prev_board[row, col] == 0 and count == 3:
+            elif prev_board[row, col] == 1 and count == 2:
                 new_board[row, col] = 1
     return new_board
 
+
 def main():
     board = load_board(load_input())
+    corners = [(0,0), (0, board.shape[1] - 1), (board.shape[0] - 1, 0), (board.shape[0] - 1, board.shape[1] - 1)]
+    for c in corners:
+        board[c] = 1
     print(board)
     for i in range(100):
         board = step_board(board)
+        for c in corners:
+            board[c] = 1
         print('step', i)
         #print(board)
     print(board_sum(board))

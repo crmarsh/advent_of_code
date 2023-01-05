636a50ad818af7bd053103c7a7a8b34248a07aa7
Chris Marsh <c.marshian@gmail.com>
2023-01-05 14:10:29 -0800

    clean up, add types

--- 2022/day23/main.py
+++ 2022/day23/main.py
@@ -5,7 +5,7 @@ from enum import Enum, IntEnum
 import os
 import pathlib
 import sys
-from typing import Tuple
+import tqdm
 
 
 here = pathlib.Path(os.path.dirname(__file__))
@@ -53,58 +53,58 @@ moves = [
 @dataclass
 class Board:
     elves: set[Point2D] = field(default_factory=set)
-    x_range: Range = field(default_factory=Range)
-    y_range: Range = field(default_factory=Range)
+    bounds: BoundingBox2D = field(default_factory=BoundingBox2D)
     step_count: int = 0
 
-    def add_elf(self, p: Point2D):
+    def add_elf(self, p: Point2D) -> None:
         self.elves.add(p)
 
-    def rebuild_ranges(self):
-        self.x_range.clear()
-        self.y_range.clear()
+    def rebuild_ranges(self) -> None:
+        self.bounds.clear()
         for elf in self.elves:
-            self.x_range.expand_to(elf.x)
-            self.y_range.expand_to(elf.y)
+            self.bounds.expand_to(elf)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         n = len(self.elves)
-        xr = len(self.x_range)
-        yr = len(self.y_range)
+        xr = len(self.bounds.x_axis)
+        yr = len(self.bounds.y_axis)
         return f"<Board round:{self.step_count}, elves:{n}, xr:{xr}, yr:{yr}, empty:{xr*yr-n}>"
 
-    def render(self):
+    def render(self) -> None:
+        print()
         self.rebuild_ranges()
-        for row in self.y_range:
-            for col in self.x_range:
-                if Point2D(col, row) in self.elves:
-                    print("#", end="")
-                else:
-                    print(".", end="")
+        for row in self.bounds.y_axis:
+            for col in self.bounds.x_axis:
+                c = "#" if Point2D(col, row) in self.elves else "."
+                print(c, end="")
             print()
         print(self)
 
-    def step(self):
+    def step(self) -> int:
+        # first propose moves
         proposals = {}
         proposal_count = {}
         for elf in self.elves:
+            # check all 8 directions around the elf
             openings = {}
             for f in Facing:
                 p = elf + forward[f.value]
                 if p not in self.elves:
                     openings[f] = p
+            # if all are open, or if not enough are open, skip it
             if len(openings) == 8 or len(openings) < 3:
                 continue
             proposal = None
+            # look though the potential moves and pick the first one that is valid
             for i in range(len(moves)):
                 j = (i + self.step_count) % len(moves)
                 proposed_dir, dirs_to_check = moves[j]
                 if all([((elf + forward[x]) not in self.elves) for x in dirs_to_check]):
                     proposal = elf + forward[proposed_dir]
+                    proposal_count[proposal] = proposal_count.get(proposal, 0) + 1
+                    proposals[elf] = proposal
                     break
-            if proposal is not None:
-                proposal_count[proposal] = proposal_count.get(proposal, 0) + 1
-                proposals[elf] = proposal
+        # do the proposed moves, if they don't conflict
         elves_moved = 0
         for elf in proposals:
             p = proposals[elf]
@@ -132,7 +132,7 @@ def parse_input(fn: str) -> Board:
     return b
 
 
-def main():
+def main() -> None:
     examples = [
         "sample_input.txt",
         "input.txt",
@@ -140,13 +140,19 @@ def main():
 
     for file_name in examples:
         board = parse_input(file_name)
+        progress = tqdm.tqdm()
         while True:
+            progress.set_description(f"on round {board.step_count + 1}")
             num_moved = board.step()
+            progress.update(num_moved)
             if board.step_count == 10:
                 board.render()
             if num_moved == 0:
                 break
+        stats = progress.format_dict
+        del progress
         board.render()
+        print("that took", stats["elapsed"], "seconds for", stats["n"], "moves")
 
 
 if __name__ == "__main__":
--- python_common/math_util.py
+++ python_common/math_util.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Iterator
 
 
 @dataclass(frozen=True, slots=True)
@@ -6,16 +7,16 @@ class Point2D:
     x: int = 0
     y: int = 0
 
-    def __add__(self, other):
+    def __add__(self, other: "Point2D") -> "Point2D":
         return Point2D(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "Point2D") -> "Point2D":
         return Point2D(self.x - other.x, self.y - other.y)
 
-    def manhattan_length(self):
+    def manhattan_length(self) -> int:
         return abs(self.x) + abs(self.y)
 
-    def dot(self, other):
+    def dot(self, other: "Point2D") -> int:
         return self.x * other.x + self.y * other.y
 
 
@@ -25,16 +26,16 @@ class Point3D:
     y: int = 0
     z: int = 0
 
-    def __add__(self, other):
+    def __add__(self, other: "Point3D") -> "Point3D":
         return Point3D(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "Point3D") -> "Point3D":
         return Point3D(self.x - other.x, self.y - other.y, self.z - other.z)
 
-    def manhattan_length(self):
+    def manhattan_length(self) -> int:
         return abs(self.x) + abs(self.y) + abs(self.z)
 
-    def dot(self, other):
+    def dot(self, other: "Point3D") -> int:
         return self.x * other.x + self.y * other.y + self.z * other.z
 
 
@@ -61,23 +62,26 @@ class Range:
     def __contains__(self, val: int) -> bool:
         return self.low <= val < self.too_high
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: "Range") -> bool:
         """
         Defines an ordering on Ranges to just compare the lower bound; probably only makes sense in certain limited cases
         """
         return self.low < other.low
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[int]:
         for x in range(self.low, self.too_high):
             yield x
 
-    def contains_value(self, val) -> bool:
+    def contains_value(self, val: int) -> bool:
         return self.low <= val < self.too_high
 
-    def contains_range(self, other):
+    def contains_range(self, other: "Range") -> bool:
         return self.low <= other.low < other.too_high <= self.too_high
 
-    def wrap(self, val):
+    def clamp(self, val: int) -> int:
+        return min(max(val, self.low), self.too_high - 1)
+
+    def wrap(self, val: int) -> int:
         if val in self:
             return val
         x = (val - self.low) % len(self)
@@ -85,15 +89,42 @@ class Range:
 
 
 @dataclass(slots=True)
+class BoundingBox2D:
+    x_axis: Range = field(default_factory=Range)
+    y_axis: Range = field(default_factory=Range)
+
+    def clear(self) -> None:
+        self.x_axis.clear()
+        self.y_axis.clear()
+
+    def expand_to(self, p: Point2D) -> None:
+        self.x_axis.expand_to(p.x)
+        self.y_axis.expand_to(p.y)
+
+    def __contains__(self, p: Point2D) -> bool:
+        return p.x in self.x_axis and p.y in self.y_axis
+
+    def __iter__(self) -> Iterator[Point2D]:
+        for y in self.y_axis:
+            for x in self.x_axis:
+                yield Point2D(x, y)
+
+
+@dataclass(slots=True)
 class BoundingBox3D:
-    x_axis: Range = Range()
-    y_axis: Range = Range()
-    z_axis: Range = Range()
+    x_axis: Range = field(default_factory=Range)
+    y_axis: Range = field(default_factory=Range)
+    z_axis: Range = field(default_factory=Range)
+
+    def clear(self) -> None:
+        self.x_axis.clear()
+        self.y_axis.clear()
+        self.z_axis.clear()
 
     def expand_to(self, p: Point3D) -> None:
         self.x_axis.expand_to(p.x)
         self.y_axis.expand_to(p.y)
         self.z_axis.expand_to(p.z)
 
-    def __contains__(self, p: Point3D):
+    def __contains__(self, p: Point3D) -> bool:
         return p.x in self.x_axis and p.y in self.y_axis and p.z in self.z_axis

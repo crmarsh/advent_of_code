7ebdd4cce17dc816859a711f81572d098c6ee16d
Chris Marsh <c.marshian@gmail.com>
2023-12-03 16:47:34 -0800

    2023 day 1 part 2, in Swift

--- 2023/day01/Sources/2023day01.swift
+++ 2023/day01/Sources/2023day01.swift
@@ -1,7 +1,6 @@
 import Foundation
 import ArgumentParser
 
-
 var digitStrings = [
     ("one", 1),
     ("two", 2),
@@ -17,42 +16,6 @@ var digitStrings = [
 struct Day01Solution: ParsableCommand {
     @Option(help: "Specify the input file")
     public var input: String = "sample_input.txt"
-
-    public func transformPrefix(of: inout String, at: String.Index, from: String, to: Character) {
-        let charsLeft = of.distance(from:at, to:of.endIndex)
-        if charsLeft < from.length {
-            return
-        }
-
-        let endFromIndex = of.index(at, offsetBy: from.length)
-        if of[at..<endFromIndex] != from {
-            return
-        }
-
-        //print("removing \(of) \(at) \(endFromIndex) \(from) \(to)")
-
-        of.replaceSubrange(at...at, with: repeatElement(to, count: 1))
-        let newAt = of.index(after: at)
-        of.removeSubrange(newAt..<endFromIndex)
-    }
-
-  public func transformDigits(of: String) -> String {
-    var replaced = of
-    var index = replaced.index(replaced.startIndex, offsetBy: 0)
-    while index < replaced.endIndex {
-        transformPrefix(of: &replaced, at: index, from:"one",   to:"1")
-        transformPrefix(of: &replaced, at: index, from:"two",   to:"2")
-        transformPrefix(of: &replaced, at: index, from:"three", to:"3")
-        transformPrefix(of: &replaced, at: index, from:"four",  to:"4")
-        transformPrefix(of: &replaced, at: index, from:"five",  to:"5")
-        transformPrefix(of: &replaced, at: index, from:"six",   to:"6")
-        transformPrefix(of: &replaced, at: index, from:"seven", to:"7")
-        transformPrefix(of: &replaced, at: index, from:"eight", to:"8")
-        transformPrefix(of: &replaced, at: index, from:"nine",  to:"9")
-        index = replaced.index(after: index)
-    }
-    return replaced
-  }
 
     public func firstDigit(of: String) -> Int {
         var index = of.startIndex
@@ -101,27 +64,12 @@ struct Day01Solution: ParsableCommand {
     
         var total: Int = 0
         while let line = readLine() {
-            print(line)
-            let fd = firstDigit(of: line)
-            let ld = lastDigit(of: line)
-            print(fd, ld)
-            let changedLine = transformDigits(of: line)
-            print(changedLine)
-
-            var firstDigit: Int? = nil
-            var lastDigit: Int? = nil
-            for c in changedLine {
-                let d = c.wholeNumberValue
-                if d != nil {
-                    lastDigit = d
-                    if firstDigit == nil {
-                        firstDigit = d
-                    }
-                }
-            }
-
-            let lineVal = 10 * firstDigit! + lastDigit!
-            print("lineVal \(lineVal)\n")
+            //print(line)
+            let firstDigit = firstDigit(of: line)
+            let lastDigit = lastDigit(of: line)
+            
+            let lineVal = 10 * firstDigit + lastDigit
+            //print("lineVal \(lineVal)\n")
             total += lineVal
         }
         print("total \(total)")

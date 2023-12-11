204ec43763fab58d32c9952ce0901712339d84cf
Chris Marsh <c.marshian@gmail.com>
2023-12-10 18:11:27 -0800

    2023 day 2 part 2

--- 2023/day02/main.js
+++ 2023/day02/main.js
@@ -10,10 +10,16 @@ const lineRe = /^Game (\d+): (.*)/;
 const entryRe = /(\d+) (red|green|blue)/g;
 const bagLimits = { 'red': 12, 'green': 13, 'blue': 14 };
 
+const cubeSetPower = (limits) => {
+    const { red, green, blue } = limits;
+    return red * green * blue;
+};
+
 (async () => {
     const file = await open(inputToLoad);
 
     let legitGameSum = 0;
+    let powerSum = 0;
 
     for await (const line of file.readLines()) {
         const match = lineRe.exec(line);
@@ -21,38 +27,38 @@ const bagLimits = { 'red': 12, 'green': 13, 'blue': 14 };
             continue;
         }
         let gameIsLegit = true;
+        let gameLimits = { 'red': 0, 'green': 0, 'blue': 0 };
         const gameNum = parseInt(match[1]);
         const rest = match[2].split(";");
-        //console.log(gameNum);
 
         for (var round of rest) {
-            //console.log(round);
             gameRound = {};
             let r = round.matchAll(entryRe);
             for (var entry of r) {
                 const count = parseInt(entry[1]);
                 const color = entry[2];
                 gameRound[color] = count;
+                gameLimits[color] = Math.max(gameLimits[color], count)
             }
-            //console.log(gameRound);
             let isLegit = true;
             for (var key in gameRound) {
                 if (gameRound.hasOwnProperty(key)) {
                     if (gameRound[key] > bagLimits[key]) {
                         isLegit = false;
-                        break;
                     }
                 }
             }
             if (!isLegit) {
                 gameIsLegit = false;
-                break;
             }
         }
+
+        powerSum += cubeSetPower(gameLimits);
 
         if (gameIsLegit) {
             legitGameSum += gameNum;
         }
     }
     console.log("legitGameSum: " + legitGameSum);
+    console.log("powerSum: " + powerSum);
 })();

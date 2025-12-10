6f07223c4f4bb45ba10d0c3a1ebacfad11004858
Chris Marsh <c.marshian@gmail.com>
2025-12-09 22:43:51 -0800

    2025 day 1 part 2

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -15,7 +15,7 @@ def load_input():
                 continue
             direction = -1 if line[0] == "L" else 1
             amount = int(line[1:])
-            moves.append(direction * amount)
+            moves.append((direction, amount))
     return moves
 
 
@@ -23,14 +23,26 @@ def main(moves):
     zeros = 0
     position = 50
     for move in moves:
-        # print(position)
-        position = (position + move) % 100
+        amt = move[0] * move[1]
+        position = (position + amt) % 100
         if position == 0:
             zeros += 1
-    # print(position)
+    print("Zeros:", zeros)
+
+
+def main2(moves):
+    zeros = 0
+    position = 50
+    for move in moves:
+        delta, amt = move
+        for i in range(amt):
+            position = (position + delta) % 100
+            if position == 0:
+                zeros += 1
     print("Zeros:", zeros)
 
 
 if __name__ == "__main__":
     moves = load_input()
     main(moves)
+    main2(moves)
